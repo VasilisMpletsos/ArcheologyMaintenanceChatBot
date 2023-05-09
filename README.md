c2d75b6a486328db748f82d473c8289b7f666ea6
Vmpletsos <vmpletsos@iti.gr>
2023-05-09 12:16:14 +0300

    Marble degredations

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Archeology Maintenance Chat Bot
 
-This is the github repository for the implementation of archeological preservation bot in the part of Palimpsisto project.
+This is the github repository for the implementation of archeological preservation bot that is part of Palimpsisto project.
--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,175 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import datetime
+from random import randint, choice
+from typing import Any, Text, Dict, List
+from rasa_sdk import Tracker, Action
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import (
+    SlotSet,
+    EventType,
+)
+
+class ActionSetStructureMarbleSlot(Action):
+    """Set the marble slot structure"""
+
+    def name(self) -> Text:
+        return "action_set_structure_marble"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        dispatcher.utter_message(response="utter_confirm_marble_material")
+        return [SlotSet("structure_marble", True), SlotSet("structure_stone", False), SlotSet("structure_mortar", False), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", False)]
+    
+class ActionSetStructureStoneSlot(Action):
+    """Set the stone slot structure"""
+
+    def name(self) -> Text:
+        return "action_set_structure_stone"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        dispatcher.utter_message(response="utter_confirm_stone_material")
+        return [SlotSet("structure_marble", False), SlotSet("structure_stone", True), SlotSet("structure_mortar", False), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", False)]
+    
+class ActionSetStructureMortarSlot(Action):
+    """Set the mortar slot structure"""
+
+    def name(self) -> Text:
+        return "action_set_structure_mortar"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        dispatcher.utter_message(response="utter_confirm_mortar_material")
+        return [SlotSet("structure_marble", False), SlotSet("structure_stone", False), SlotSet("structure_mortar", True), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", False)]
+    
+class ActionSetStructureShaleSlot(Action):
+    """Set the shale slot structure"""
+
+    def name(self) -> Text:
+        return "action_set_structure_shale"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        dispatcher.utter_message(response="utter_confirm_shale_material")
+        return [SlotSet("structure_marble", False), SlotSet("structure_stone", False), SlotSet("structure_mortar", False), SlotSet("structure_shale", True), SlotSet("structure_conch_shells", False)]
+    
+class ActionSetStructureConchSlot(Action):
+    """Set the shale conch shells structure"""
+
+    def name(self) -> Text:
+        return "action_set_structure_conch_shells"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        dispatcher.utter_message(response="utter_confirm_conch_shells_material")
+        return [SlotSet("structure_marble", False), SlotSet("structure_stone", False), SlotSet("structure_mortar", False), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", True)]
+    
+class ActionEvaluateSlowDegradationRate(Action):
+    """Respond to slow degredation rate"""
+
+    def name(self) -> Text:
+        return "action_recommend_for_slow_degradation_rate"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        today = datetime.datetime.now()
+        random_delta = randint(30, 180)
+        recommended_date = today + datetime.timedelta(days=random_delta)
+        message = 'Η συντήρηση να γίνει μέχρι τις ' + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_marble"):
+        #     message = choice(marble_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_stone"):
+        #     message = choice(stone_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_mortar"):
+        #     message = choice(mortar_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_shale"):
+        #     message = choice(shale_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_conch_shells"):
+        #     message = choice(conch_shells_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        dispatcher.utter_message(message)
+        return
+    
+class ActionEvaluateMediumDegradationRate(Action):
+    """Respond to medium degredation rate"""
+
+    def name(self) -> Text:
+        return "action_recommend_for_medium_degradation_rate"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        today = datetime.datetime.now()
+        random_delta = randint(30, 90)
+        recommended_date = today + datetime.timedelta(days=random_delta)
+        message = 'Η συντήρηση να γίνει μέχρι τις ' + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_marble"):
+        #     message = choice(marble_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_stone"):
+        #     message = choice(stone_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_mortar"):
+        #     message = choice(mortar_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_shale"):
+        #     message = choice(shale_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_conch_shells"):
+        #     message = choice(conch_shells_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        dispatcher.utter_message(message)
+        return
+    
+class ActionEvaluateFastDegradationRate(Action):
+    """Respond to fast degredation rate"""
+
+    def name(self) -> Text:
+        return "action_recommend_for_fast_degradation_rate"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        today = datetime.datetime.now()
+        random_delta = randint(10, 30)
+        recommended_date = today + datetime.timedelta(days=random_delta)
+        message = 'Η συντήρηση να γίνει μέχρι τις ' + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_marble"):
+        #     message = choice(marble_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_stone"):
+        #     message = choice(stone_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_mortar"):
+        #     message = choice(mortar_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_shale"):
+        #     message = choice(shale_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        # if tracker.get_slot("structure_conch_shells"):
+        #     message = choice(conch_shells_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        dispatcher.utter_message(message)
+        return
