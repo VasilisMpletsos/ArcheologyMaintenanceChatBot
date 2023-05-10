fb2c2c1c8d5e759d95f4e0e724ca388fffaf19ab
Vmpletsos <vmpletsos@iti.gr>
2023-05-10 15:00:17 +0300

    Custom actions for window of action for restoration

--- actions/actions.py
+++ actions/actions.py
@@ -5,8 +5,6 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 import datetime
 from random import randint, choice
 from typing import Any, Text, Dict, List
@@ -18,80 +16,6 @@ from rasa_sdk.events import (
     EventType,
 )
 
-class ActionSetStructureMarbleSlot(Action):
-    """Set the marble slot structure"""
-
-    def name(self) -> Text:
-        return "action_set_structure_marble"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> List[EventType]:
-        dispatcher.utter_message(response="utter_confirm_marble_material")
-        return [SlotSet("structure_marble", True), SlotSet("structure_stone", False), SlotSet("structure_mortar", False), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", False)]
-    
-class ActionSetStructureStoneSlot(Action):
-    """Set the stone slot structure"""
-
-    def name(self) -> Text:
-        return "action_set_structure_stone"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> List[EventType]:
-        dispatcher.utter_message(response="utter_confirm_stone_material")
-        return [SlotSet("structure_marble", False), SlotSet("structure_stone", True), SlotSet("structure_mortar", False), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", False)]
-    
-class ActionSetStructureMortarSlot(Action):
-    """Set the mortar slot structure"""
-
-    def name(self) -> Text:
-        return "action_set_structure_mortar"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> List[EventType]:
-        dispatcher.utter_message(response="utter_confirm_mortar_material")
-        return [SlotSet("structure_marble", False), SlotSet("structure_stone", False), SlotSet("structure_mortar", True), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", False)]
-    
-class ActionSetStructureShaleSlot(Action):
-    """Set the shale slot structure"""
-
-    def name(self) -> Text:
-        return "action_set_structure_shale"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> List[EventType]:
-        dispatcher.utter_message(response="utter_confirm_shale_material")
-        return [SlotSet("structure_marble", False), SlotSet("structure_stone", False), SlotSet("structure_mortar", False), SlotSet("structure_shale", True), SlotSet("structure_conch_shells", False)]
-    
-class ActionSetStructureConchSlot(Action):
-    """Set the shale conch shells structure"""
-
-    def name(self) -> Text:
-        return "action_set_structure_conch_shells"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> List[EventType]:
-        dispatcher.utter_message(response="utter_confirm_conch_shells_material")
-        return [SlotSet("structure_marble", False), SlotSet("structure_stone", False), SlotSet("structure_mortar", False), SlotSet("structure_shale", False), SlotSet("structure_conch_shells", True)]
     
 class ActionEvaluateSlowDegradationRate(Action):
     """Respond to slow degredation rate"""
@@ -108,17 +32,8 @@ class ActionEvaluateSlowDegradationRate(Action):
         today = datetime.datetime.now()
         random_delta = randint(30, 180)
         recommended_date = today + datetime.timedelta(days=random_delta)
-        message = 'Η συντήρηση να γίνει μέχρι τις ' + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_marble"):
-        #     message = choice(marble_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_stone"):
-        #     message = choice(stone_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_mortar"):
-        #     message = choice(mortar_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_shale"):
-        #     message = choice(shale_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_conch_shells"):
-        #     message = choice(conch_shells_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        message = 'The restoration is recommended to be done until ' + recommended_date.strftime("%d/%m/%Y") + ' (d/m/y)'
+
         dispatcher.utter_message(message)
         return
     
@@ -137,17 +52,7 @@ class ActionEvaluateMediumDegradationRate(Action):
         today = datetime.datetime.now()
         random_delta = randint(30, 90)
         recommended_date = today + datetime.timedelta(days=random_delta)
-        message = 'Η συντήρηση να γίνει μέχρι τις ' + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_marble"):
-        #     message = choice(marble_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_stone"):
-        #     message = choice(stone_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_mortar"):
-        #     message = choice(mortar_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_shale"):
-        #     message = choice(shale_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_conch_shells"):
-        #     message = choice(conch_shells_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        message = 'The restoration is recommended to be done until ' + recommended_date.strftime("%d/%m/%Y") + ' (d/m/y)'
         dispatcher.utter_message(message)
         return
     
@@ -166,16 +71,6 @@ class ActionEvaluateFastDegradationRate(Action):
         today = datetime.datetime.now()
         random_delta = randint(10, 30)
         recommended_date = today + datetime.timedelta(days=random_delta)
-        message = 'Η συντήρηση να γίνει μέχρι τις ' + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_marble"):
-        #     message = choice(marble_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_stone"):
-        #     message = choice(stone_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_mortar"):
-        #     message = choice(mortar_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_shale"):
-        #     message = choice(shale_slow_rate) + recommended_date.strftime("%d/%m/%Y")
-        # if tracker.get_slot("structure_conch_shells"):
-        #     message = choice(conch_shells_slow_rate) + recommended_date.strftime("%d/%m/%Y")
+        message = 'The restoration is recommended to be done until ' + recommended_date.strftime("%d/%m/%Y") + ' (d/m/y)'
         dispatcher.utter_message(message)
         return
