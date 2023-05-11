c175472349074be62755dfc93b2e827706b5c95d
Vmpletsos <vmpletsos@iti.gr>
2023-05-11 14:59:41 +0300

    Stone unkown intents in csv

--- actions/actions.py
+++ actions/actions.py
@@ -6,6 +6,7 @@
 
 
 import datetime
+import os
 from random import randint, choice
 from typing import Any, Text, Dict, List
 from rasa_sdk import Tracker, Action
@@ -32,7 +33,7 @@ class ActionEvaluateSlowDegradationRate(Action):
         today = datetime.datetime.now()
         random_delta = randint(90, 180)
         recommended_date = today + datetime.timedelta(days=random_delta)
-        message = 'The restoration is recommended to be done until somwhere ' + recommended_date.strftime("%d/%m/%Y") + ' (d/m/y)'
+        message = 'The restoration is recommended to be done until somewhere ' + recommended_date.strftime("%d/%m/%Y") + ' (d/m/y)'
 
         dispatcher.utter_message(message)
         return
@@ -73,4 +74,23 @@ class ActionEvaluateFastDegradationRate(Action):
         recommended_date = today + datetime.timedelta(days=random_delta)
         message = 'The restoration is recommended to be done as soon as possible until ' + recommended_date.strftime("%d/%m/%Y") + ' (d/m/y)'
         dispatcher.utter_message(message)
+        return
+    
+class ActionSaveUnkownIntent(Action):
+    """Save the unknown intent"""
+
+    def name(self) -> Text:
+        return "action_save_unknown_intent"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        last_message = tracker.latest_message['text']
+        with open('unkown_intents.csv', 'a+', newline='') as f:
+            f.write(last_message)
+            f.write("\n")
+            f.close()
         return
